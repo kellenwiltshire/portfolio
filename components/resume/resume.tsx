b2f0c5c8573a7b291086365e7e6f4455550850d7
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-11-27 13:03:35 -0500

    commit to move to wsl env

---
+++ components/resume/resume.tsx
@@ -0,0 +1,134 @@
+import { Fragment } from 'react';
+
+const activity = [
+	{
+		id: 1,
+		type: 'comment',
+
+		person: { name: 'Superna', href: '#' },
+		imageUrl: 'superna.svg',
+		comment:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam cursus quis sed purus nam. ',
+		date: '6d ago',
+	},
+
+	{
+		id: 2,
+		type: 'comment',
+		person: { name: 'Jason Meyers', href: '#' },
+		imageUrl:
+			'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
+		comment:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam cursus quis sed purus nam. Scelerisque amet elit non sit ut tincidunt condimentum. Nisl ultrices eu venenatis diam.',
+		date: '2h ago',
+	},
+];
+
+function classNames(...classes) {
+	return classes.filter(Boolean).join(' ');
+}
+
+const Resume = () => {
+	return (
+		<div className='flow-root'>
+			<ul role='list' className='-mb-8'>
+				{activity.map((activityItem, activityItemIdx) => (
+					<li key={activityItem.id}>
+						<div className='relative pb-8'>
+							{activityItemIdx !== activity.length - 1 ? (
+								<span className='absolute top-5 left-5 -ml-px h-full w-0.5 bg-gray-200' aria-hidden='true' />
+							) : null}
+							<div className='relative flex items-start space-x-3'>
+								{activityItem.type === 'comment' ? (
+									<>
+										<div className='relative'>
+											<img
+												className='flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 ring-8 ring-white'
+												src={activityItem.imageUrl}
+												alt=''
+											/>
+
+											<span className='absolute -bottom-0.5 -right-1 rounded-tl bg-white px-0.5 py-px'>\</span>
+										</div>
+										<div className='min-w-0 flex-1'>
+											<div>
+												<div className='text-sm'>
+													<a href={activityItem.person.href} className='font-medium text-gray-900'>
+														{activityItem.person.name}
+													</a>
+												</div>
+												<p className='mt-0.5 text-sm text-gray-500'>Commented {activityItem.date}</p>
+											</div>
+											<div className='mt-2 text-sm text-gray-700'>
+												<p>{activityItem.comment}</p>
+											</div>
+										</div>
+									</>
+								) : activityItem.type === 'assignment' ? (
+									<>
+										<div>
+											<div className='relative px-1'>
+												<div className='flex h-8 w-8 items-center justify-center rounded-full bg-gray-100 ring-8 ring-white'></div>
+											</div>
+										</div>
+										<div className='min-w-0 flex-1 py-1.5'>
+											<div className='text-sm text-gray-500'>
+												<a href={activityItem.person.href} className='font-medium text-gray-900'>
+													{activityItem.person.name}
+												</a>{' '}
+												assigned{' '}
+												<a href={activityItem.assigned.href} className='font-medium text-gray-900'>
+													{activityItem.assigned.name}
+												</a>{' '}
+												<span className='whitespace-nowrap'>{activityItem.date}</span>
+											</div>
+										</div>
+									</>
+								) : activityItem.type === 'tags' ? (
+									<>
+										<div>
+											<div className='relative px-1'>
+												<div className='flex h-8 w-8 items-center justify-center rounded-full bg-gray-100 ring-8 ring-white'></div>
+											</div>
+										</div>
+										<div className='min-w-0 flex-1 py-0'>
+											<div className='text-sm leading-8 text-gray-500'>
+												<span className='mr-0.5'>
+													<a href={activityItem.person.href} className='font-medium text-gray-900'>
+														{activityItem.person.name}
+													</a>{' '}
+													added tags
+												</span>{' '}
+												<span className='mr-0.5'>
+													{activityItem.tags.map((tag) => (
+														<Fragment key={tag.name}>
+															<a
+																href={tag.href}
+																className='relative inline-flex items-center rounded-full border border-gray-300 px-3 py-0.5 text-sm'
+															>
+																<span className='absolute flex flex-shrink-0 items-center justify-center'>
+																	<span
+																		className={classNames(tag.color, 'h-1.5 w-1.5 rounded-full')}
+																		aria-hidden='true'
+																	/>
+																</span>
+																<span className='ml-3.5 font-medium text-gray-900'>{tag.name}</span>
+															</a>{' '}
+														</Fragment>
+													))}
+												</span>
+												<span className='whitespace-nowrap'>{activityItem.date}</span>
+											</div>
+										</div>
+									</>
+								) : null}
+							</div>
+						</div>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default Resume;
